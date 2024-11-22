dcbbde57ea8ea4e64a008747cbece6e9e4726120
norikenpi <norikenpi@gmail.com>
2024-11-22 19:57:01 +0900

    feat: add answer limit

--- python/main.py
+++ python/main.py
@@ -5,19 +5,34 @@ import random
 
 class number_guessing_game():
     def __init__(self):
-        secret_number = 42
+        self._secret_number = 42 #random.randint(1,100)
+        self._answer_limit = 7
+        self._count = 0
+        self._is_success = False
 
     def start(self):
-        status = 1
-        return status
+        for i in range(self._answer_limit):
+            input = 1
+            self._count += 1
+            self.answer_check(input)
+        return 
         
-
+    def answer_check(self, input):
+        if self._secret_number == input:
+            self._is_success = True
+        else:
+            print(f"{input} is not correct")
+            
+    def final_check(self, status):
+        if self._is_success == True:
+            print(f"Secret number is {new_game._secret_number}")
+            print("finish normally")
+        else:
+            print("finish abnormally")
+            pass
 
 if __name__ == "__main__":
-    new_game = number_guessing_game()
-    status = new_game.start()
-    if status == 1:
-        print("finish normally")
-    else:
-        print("finish abnormally")
+    game = number_guessing_game()
+    game.start()
+    game.final_check(game)
 
