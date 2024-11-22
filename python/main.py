8d90e09ff376b6990f9d6444ff7ca8615d43ef5e
norikenpi <norikenpi@gmail.com>
2024-11-22 20:10:30 +0900

    refactor

--- python/main.py
+++ python/main.py
@@ -9,13 +9,14 @@ class number_guessing_game():
         self._answer_limit = 7
         self._count = 0
         self._is_success = False
+        self.start()
 
     def start(self):
         for i in range(self._answer_limit):
             input = 1
             self._count += 1
             self.answer_check(input)
-        return 
+        self.final_check()
         
     def answer_check(self, input):
         if self._secret_number == input:
@@ -23,16 +24,14 @@ class number_guessing_game():
         else:
             print(f"{input} is not correct")
             
-    def final_check(self, status):
+    def final_check(self):
         if self._is_success == True:
-            print(f"Secret number is {new_game._secret_number}")
+            print("Congratulations!!")
             print("finish normally")
         else:
-            print("finish abnormally")
+            print(f"Secret number is {self._secret_number}.")
             pass
 
 if __name__ == "__main__":
     game = number_guessing_game()
-    game.start()
-    game.final_check(game)
 
