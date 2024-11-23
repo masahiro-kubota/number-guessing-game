cb8702c86357731e9a54c62a58fdf202c47f57e4
norikenpi <norikenpi@gmail.com>
2024-11-23 21:54:56 +0900

    feat: add answer check feature

--- python/asyncio/main.py
+++ python/asyncio/main.py
@@ -1,4 +1,17 @@
 #! /usr/bin/env python3
 
+
+class number_guessing_game():
+    def __init__(self):
+        self._secret_number = 42
+        self.validate_input()
+
+    def validate_input(self):
+        player_input = int(input())
+        if player_input == self._secret_number:
+            print("Correct")
+        else:
+            print("incorrect") 
+
 if __name__ == "__main__":
-    print("Hello")
+    game = number_guessing_game()
