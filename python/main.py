70407ae568a8b2a14c54ab704c9690d1cf20a7fa
norikenpi <norikenpi@gmail.com>
2024-11-23 13:57:34 +0900

    feat: add high low and closer farter message

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from enum import Enum
 import random
 
 
@@ -14,6 +15,7 @@ class number_guessing_game():
         self.start()
 
     def start(self):
+        print("Please input a integer between 1 and 100.")
         for i in range(self._answer_limit):
             user_answer = self.validate_input()
             self._present_answer = user_answer
@@ -28,7 +30,7 @@ class number_guessing_game():
     def validate_input(self):
         while True:
             try:
-                user_answer = float(input("Please input a integer between 1 and 100.\n"))
+                user_answer = float(input())
             except ValueError:
                 print("Please input a number")
             else:
@@ -44,23 +46,39 @@ class number_guessing_game():
     def answer_check(self, user_answer):
         if self._secret_number == user_answer:
             self._is_success = True
-        else:
-            print(f"{user_answer} is not correct")
-            
 
     def give_hint(self, user_answer):
-        # TODO high or lowの判定 
-        if self._count == 0:
-            return
+        # TODO 全角数字を入力したときの判定
+        if self._count != 0:
+            closer_farther_hint = self.give_closer_farther_hint(user_answer)
+            higher_lower_hint = self.give_higher_lower_hint(user_answer)
+            print(f"{closer_farther_hint} {higher_lower_hint}")
         else:
-            previous_distance = abs(self._previous_answer - self._secret_number)
-            present_distance = abs(self._present_answer - self._secret_number)
-            if previous_distance > present_distance:
-                print("Getting closer!")
-            elif previous_distance < present_distance:
-                print("Getting farther!")
-            elif previous_distance == present_distance:
-                print("The distance has not changed")
+            higher_lower_hint = self.give_higher_lower_hint(user_answer)
+            print(higher_lower_hint)
+
+    def give_closer_farther_hint(self, user_answer):
+        previous_distance = abs(self._previous_answer - self._secret_number)
+        present_distance = abs(self._present_answer - self._secret_number)
+        closer_farther_comparison = compare(present_distance, previous_distance)
+        if closer_farther_comparison == comparison_result.LOW:
+            closer_farther_hint = "Getting closer!"
+        elif closer_farther_comparison == comparison_result.HIGH:
+            closer_farther_hint = "Getting farther!"
+        elif closer_farther_comparison == comparison_result.EQUAL:
+            closer_farther_hint = "The distance has not changed!"
+        return closer_farther_hint
+
+    def give_higher_lower_hint(self, user_answer):
+        higher_lower_comparison = compare(self._secret_number, user_answer)
+        if higher_lower_comparison == comparison_result.LOW:
+            higher_lower_hint = "Aim for a low number!"
+        elif higher_lower_comparison == comparison_result.HIGH:
+            higher_lower_hint = "Aim for a high number!"
+        elif higher_lower_comparison == comparison_result.EQUAL:
+            # Following message never show.
+            higher_lower_hint = "Now you know the correct answer, right?"
+        return higher_lower_hint
 
     def final_check(self):
         if self._is_success == True:
@@ -68,6 +86,19 @@ class number_guessing_game():
         else:
             print(f"Secret number is {self._secret_number}.")
             pass
+
+class comparison_result(Enum):
+    LOW = -1
+    EQUAL = 0
+    HIGH = 1
+
+def compare(a, b):
+    if a < b:
+        return comparison_result.LOW
+    elif a > b:
+        return comparison_result.HIGH
+    else:
+        return comparison_result.EQUAL
 
 if __name__ == "__main__":
     game = number_guessing_game()
