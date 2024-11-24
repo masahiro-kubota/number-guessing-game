caabc9c74a3839f75688f362d94ab01255b5b9bd
norikenpi <norikenpi@gmail.com>
2024-11-24 20:43:46 +0900

    feat: user input is refused after the time limit

--- python/asyncio/main.py
+++ python/asyncio/main.py
@@ -4,6 +4,8 @@ import asyncio
 from time import time, sleep
 import random
 
+import aioconsole
+
 global start_time
 
 class number_guessing_game():
@@ -14,32 +16,42 @@ class number_guessing_game():
 
     async def async_main(self):
         loop = asyncio.get_running_loop()
-        tasks = [loop.run_in_executor(None, self.validate_input), loop.run_in_executor(None, self.provide_hint)]
+        task1 = self.validate_input()
+        task2 = self.provide_hint()
+        tasks = [task1, task2]
         try:
-            async with asyncio.timeout(10):
+            async with asyncio.timeout(4):
                 await asyncio.gather(*tasks)
+        except asyncio.CancelledError:
+            print("Task is being Cancelled, cleaning up....")
         except TimeoutError:
-            loop.stop()
-            print("Time Out!")
+            try: 
+                loop.close()
+            except RuntimeError:
+                print("Time Out!")
 
-    def validate_input(self):
-        start_validate_time = time()
-        i = 0
-        while True:
-            i += 1
-            player_input = int(input())
-            if player_input == self._secret_number:
-                print("Correct")
-            else:
-                print("incorrect") 
-            finish_validate_time = time()
-            print(f"elapsed input time{i}: {finish_validate_time - start_validate_time}")
+    async def validate_input(self):
+        try:
+            start_validate_time = time()
+            i = 0
+            while True:
+                i += 1
+                player_input_raw = await aioconsole.ainput()
+                player_input = int(player_input_raw)
+                if player_input == self._secret_number:
+                    print("Correct")
+                else:
+                    print("incorrect") 
+                finish_validate_time = time()
+                print(f"elapsed input time{i}: {finish_validate_time - start_validate_time}")
+        except asyncio.CancelledError:
+            pass
 
-    def provide_hint(self):
-        for i in range(10):
+    async def provide_hint(self):
+        for i in range(4):
             # start_hint_time = time()
             print("Secret number is an even number!") if self._secret_number%2==0 else print("Secret number is an odd number!")
-            sleep(1)
+            await asyncio.sleep(1)
             finish_hint_time = time()
             print(f"elapsed hint time{i}: {finish_hint_time - start_time}") 
 
