f1769d90a57b91f5faf478b2d30905cde0ae7448
norikenpi <norikenpi@gmail.com>
2024-12-08 23:03:13 +0900

    refactor

--- javascript/simple/script.js
+++ javascript/simple/script.js
@@ -1,10 +1,12 @@
-// inputNumFormのsubmitボタンが押されるとhanlerFormSubmitがイベントコールされる 
-// eventは送信されたeventオブジェクト
-document.getElementById("inputNumForm").addEventListener("submit", handleFormSubmit);
-
 function handleFormSubmit(event){
   event.preventDefault();
   console.log(event.type);
 }
 
-    
+function main(){
+  // inputNumFormのsubmitボタンが押されるとhanlerFormSubmitがイベントコールされる 
+  // eventは送信されたeventオブジェクト
+  document.getElementById("inputNumForm").addEventListener("submit", handleFormSubmit);
+}
+
+window.addEventListener("DOMContentLoaded", main);
