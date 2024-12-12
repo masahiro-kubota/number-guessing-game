42880f4b50ee1769889efc31aa34bd32095e2bac
Masahiro Kubota <norikenpi@gmail.com>
2024-12-12 23:58:58 +0900

    add class

--- javascript/simple/script.js
+++ javascript/simple/script.js
@@ -1,18 +1,32 @@
+class Game{
+  constructor(secret_number){
+    this._secret_number = secret_number;
+  }
+
+  get secret_number(){
+    return this._secret_number;
+  }
+}
+
+
+
+
+
 function handleFormSubmit(event){
   event.preventDefault();
   const info = document.getElementById("info");
   const input_num  = document.getElementById("input_num").value;
-  if (input_num == secret_number) {
+  if (input_num == game.secret_number) {
     info.textContent = `${input_num} is secret number`;
   } else {
     info.textContent = `${input_num} is not secret number`;
   }
 }
 
-const secret_number = 80;
-
+let game;
 
 function main(){
+  game = new Game(49);
   // inputNumFormのsubmitボタンが押されるとhanlerFormSubmitがイベントコールされる 
   // eventは送信されたeventオブジェクト
  document.getElementById("inputNumForm").addEventListener("submit", handleFormSubmit);
