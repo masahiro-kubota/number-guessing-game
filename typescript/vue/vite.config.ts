bc1694cf066eb609360c48c6697dccfc5e247cd4
norikenpi <norikenpi@gmail.com>
2024-12-16 01:28:30 +0900

    add vite-plugin-checker

--- typescript/vue/vite.config.ts
+++ typescript/vue/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  root: '.', // プロジェクトのルート
+  server: {
+    port: 3000, // 開発サーバーのポート番号
+    },
+  plugins: [
+    vue(),
+    checker({ typescript: true })
+  ],
 })
