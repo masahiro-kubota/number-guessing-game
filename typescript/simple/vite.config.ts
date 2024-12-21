be8de4b42483f530d81936a8e4a1ba5edf6d6938
norikenpi <norikenpi@gmail.com>
2024-12-21 18:47:10 +0900

    modify config

--- typescript/simple/vite.config.ts
+++ typescript/simple/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   root: '.', // プロジェクトのルート
   server: {
     port: 3000, // 開発サーバーのポート番号
+    open: true,
     },
   plugins: [
     checker({ typescript: true }) // TypeScript の型チェックを有効化
