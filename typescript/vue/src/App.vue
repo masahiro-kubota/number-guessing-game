fd4f0bd02c98a044999708dbed27c816e0119977
norikenpi <norikenpi@gmail.com>
2024-12-21 20:40:30 +0900

    combine two files into one

--- typescript/vue/src/App.vue
+++ typescript/vue/src/App.vue
@@ -1,23 +1,47 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  }
+}
 </script>
 
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo !h-32" alt="Vite logo" />
+      <img src="/vite.svg" class="logo h-32 p-6" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue h-32 p-6" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-9xl font-thin italic animate-bounce">"Vite + Vue"</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs text-gray-400">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
