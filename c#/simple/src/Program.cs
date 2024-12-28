984f386b63b525c53f301a71dc11f1320ca7bae0
norikenpi <norikenpi@gmail.com>
2024-12-28 15:45:03 +0900

    add class

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Person {
+  private string name;
+  private int age;
+
+  public string Name {
+    get { return name; }
+    set { name = value; }
+  }
+
+  public int Age{
+    get { return age; }
+    set { age = value; }
+  }
+
+  public Person(string name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void Greet() {
+    Console.WriteLine($"Hello {name}");
+  }
+}
+
+class Program {
+  static void Main(string[] args) {
+    Console.WriteLine("Hello world!");
+    Person person = new Person("Bob", 20);
+    person.Greet();
+  }
+}
