50db2189331a5ac97e68d6bc30feef21207a8380
norikenpi <norikenpi@gmail.com>
2024-12-29 23:40:05 +0900

    arrange static method

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -5,6 +5,9 @@ namespace MySimpleApp {
         public static string ReturnInput(string input) {
             return input;
        }
+        public static ValidationResult<string> IsInteger(string input) {
+            return ValidationResult<string>.Success("success");
+        }
     }
 
     public class CLIInput {
@@ -18,7 +21,7 @@ namespace MySimpleApp {
         static void Main(string[] args) {
             string input = CLIInput.GetInput();
             Console.WriteLine(ValidateInput.ReturnInput(input));
-            var result = ValidationResult<string>.Success("success");
+            var result = ValidateInput.IsInteger(input);
             if (result.IsValid) {
                 Console.WriteLine(result.Value);
             }
