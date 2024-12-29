91fb1e59d24aa239060083f13259006cf56e7a33
norikenpi <norikenpi@gmail.com>
2024-12-29 23:33:15 +0900

    add static method

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -2,16 +2,22 @@
 
 namespace MySimpleApp {
     public class ValidateInput {
-          public ValidateInput() {
-              string name = Console.ReadLine();
-              Console.WriteLine(name);
+        public static string ReturnInput(string input) {
+            return input;
        }
+    }
+
+    public class CLIInput {
+        public static string GetInput() { 
+            Console.WriteLine("Please enter input: ");
+            return Console.ReadLine();
+        }
     }
 
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello world!");
-            ValidateInput validateInput = new ValidateInput();
+            string input = CLIInput.GetInput();
+            Console.WriteLine(ValidateInput.ReturnInput(input));
             var result = ValidationResult<string>.Success("success");
             if (result.IsValid) {
                 Console.WriteLine(result.Value);
