4554e432424644cf1384ecd642580001a59f53d8
norikenpi <norikenpi@gmail.com>
2025-01-01 12:56:53 +0900

    make all functions immutable

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -2,39 +2,30 @@
 
     public class GameSetting {
         // 全てイミュータブルなので、getterでのディープコピーは必要ない。
-        public int SecretNumber {get; set;}
-        public int MaxAttempts {get; set;}
-        public GameSetting(int secretNumber, int maxAttempts) {
-            SecretNumber = secretNumber;
-            MaxAttempts = maxAttempts;
-        }
+        public int SecretNumber {get; init;}
+        public int MaxAttempts {get; init;}
     }
 
     public class GameState {
         // 全てイミュータブルなので、getterでのディープコピーは必要ない。
-        public int? LastInputNumber {get; set;}
-        public int CurrentAttempt {get; set;}
-        public bool IsSuccess {get; set;}
-        public GameState() {
-            LastInputNumber = null;
-            CurrentAttempt = 0;
-            IsSuccess = false;
-        }           
-        // 再生成して不変性を維持する必要がある。
-        public GameState UpdateState(int input, GameSetting gameSetting) {
-            LastInputNumber = input;
-            CurrentAttempt++;
-            IsSuccess = (gameSetting.SecretNumber == input) ? true : false;
-            return this;
+        public int? LastInputNumber {get; init;} = null;
+        public int CurrentAttempt {get; init;} = 0;
+        public bool IsSuccess {get; init;} = false;
+        // 大した容量でもないし頻度も低いので再生成しても問題ない。
+        public static GameState UpdateState(int input, GameState gameState, GameSetting gameSetting) {
+            return new GameState {
+                LastInputNumber = input,
+                CurrentAttempt = gameState.CurrentAttempt + 1,
+                IsSuccess = (gameSetting.SecretNumber == input) ? true : false
+            };
         }
     }
 
 
     //InputStateというのも作ろうと思ったがintで問題ないので、GameManagerの中でintで状態を定義
-
     public class GameManager {
         public GameManager() {
-            GameSetting gameSetting = new GameSetting(43, 7); 
+            GameSetting gameSetting = new GameSetting{SecretNumber = 43, MaxAttempts = 7}; 
             GameState gameState = new GameState();
             while (!gameState.IsSuccess && gameState.CurrentAttempt < gameSetting.MaxAttempts) {
                 gameState = ProcessAttempt(gameSetting, gameState);
@@ -42,26 +33,25 @@
         }
         //TODO 検証関数を追加する。
         //TODO 検証内容に基づいたInterfaceを設定したい
-        public static bool IsValidInput(string? input) {
-            return int.TryParse(input, out int result);
+        public static bool IsValidInput(string? input, out int result) {
+            return int.TryParse(input, out result);
         }
 
         public static GameState ProcessAttempt(GameSetting gameSetting, GameState gameState) {
-            // successとかinputは状態なのでいい感じに扱う必要がある。
+            // successとかinputは状態なのでいい感じに扱う必要がある。stringは参照型だがイミュータブルなので不変。
             string? input = null;
-            while (!IsValidInput(input)) {
+            int parsedInput;
+            while (!IsValidInput(input, out parsedInput)) {
                 input = CliIo.CliInput();
             }
             Console.WriteLine("Your input is valid input");
-            gameState = gameState.UpdateState(int.Parse(input), gameSetting);
+            gameState = GameState.UpdateState(parsedInput, gameState, gameSetting);
             if (gameState.IsSuccess) {
                 Console.WriteLine("Your input is correct.");
             }
             return gameState;
-
         }
     }
-
 
     public class CliIo {
         public static string? CliInput() { 
