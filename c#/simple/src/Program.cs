b28755a056f13eca9211b34a63a834e22f7c517b
norikenpi <norikenpi@gmail.com>
2025-01-01 16:56:58 +0900

    new ui

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace NumberGuessingGame {
     //Domain
@@ -26,15 +27,17 @@ namespace NumberGuessingGame {
     // Business
     //InputStateというのも作ろうと思ったがintで問題ないので、GameManagerの中でintで状態を定義
     public class GameManager {
-        public GameManager(IInputData input) {
+        public GameManager(IUserInterface ui) {
             GameSetting gameSetting = new GameSetting{SecretNumber = 43, MaxAttempts = 7}; 
             GameState gameState = new GameState();
+            gameState = ProcessAttempt(ui, gameSetting, gameState);
             while (!gameState.IsSuccess && gameState.CurrentAttempt < gameSetting.MaxAttempts) {
-                gameState = ProcessAttempt(input, gameSetting, gameState);
+                ui.StandardOutput("Not Correct");
+                gameState = ProcessAttempt(ui, gameSetting, gameState);
             }
         }
 
-        public static GameState ProcessAttempt(IInputData inputData, GameSetting gameSetting, GameState gameState) {
+        public static GameState ProcessAttempt(IUserInterface inputData, GameSetting gameSetting, GameState gameState) {
             // successとかinputは状態なのでいい感じに扱う必要がある。stringは参照型だがイミュータブルなので不変。
             int parsedInput = inputData.InputInteger();
             gameState = GameState.UpdateState(parsedInput, gameState, gameSetting);
@@ -42,23 +45,24 @@ namespace NumberGuessingGame {
         }
     }
 
-   public interface IInputData {
+   public interface IUserInterface {
        string GetUser();
        int InputInteger();
        DateTime InputTime();
+       void StandardOutput(string output);
    }
 
     // Presentation
-    public class CliIo : IInputData {
+    public class CliIo : IUserInterface {
         public string GetUser() {
             return "User";
         }
         //TODO 検証関数を追加する。
         //TODO 検証内容に基づいたInterfaceを設定したい
+
         public static bool IsValidInput(string? input, out int result) {
             return int.TryParse(input, out result);
         }
-
         public int InputInteger() {
             Console.WriteLine("Please enter input: ");
             string? input = Console.ReadLine();
@@ -72,12 +76,43 @@ namespace NumberGuessingGame {
         public DateTime InputTime() {
             return DateTime.Now;
         }
+
+        public void StandardOutput(string output) { 
+            Console.WriteLine(output);
+        }
     }
+
+    public class FileIo: IUserInterface {
+        public string FilePath{get; init;} = "input.txt";
+        public string GetUser() {
+            return "User";
+        }
+        public static bool IsValidInput(string? input, out int result) {
+            return int.TryParse(input, out result);
+        }
+        public int InputInteger() {
+            string? input = File.ReadLines(FilePath).FirstOrDefault();
+            int parsedInput;
+            while (!IsValidInput(input, out parsedInput)) {
+                Console.WriteLine("Invalide Input");
+                Thread.Sleep(1000);
+            }
+            return parsedInput;
+        }
+        public DateTime InputTime() {
+            return DateTime.Now;
+        }
+        public void StandardOutput(string output) { 
+            Console.WriteLine(output);
+            File.WriteAllText(FilePath, output);
+        }
+    }
+
 
     class Program {
         static void Main(string[] args) {
             // 依存性注入 string?で型制約入れてる。
-            GameManager gameManager = new GameManager(new CliIo());
+            GameManager gameManager = new GameManager(new FileIo());
         }
     }
 }
