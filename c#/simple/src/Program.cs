8ba6c8c8fb1899b612e38efa98b7dd08c114a929
norikenpi <norikenpi@gmail.com>
2025-01-01 16:12:49 +0900

    change validation func to presentation layer

--- c#/simple/src/Program.cs
+++ c#/simple/src/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace NumberGuessingGame {
-
+    //Domain
     public class GameSetting {
         // 全てイミュータブルなので、getterでのディープコピーは必要ない。
         public int SecretNumber {get; init;}
@@ -22,8 +22,8 @@ namespace NumberGuessingGame {
             };
         }
     }
-
-
+    
+    // Business
     //InputStateというのも作ろうと思ったがintで問題ないので、GameManagerの中でintで状態を定義
     public class GameManager {
         public GameManager(IInputData input) {
@@ -33,45 +33,42 @@ namespace NumberGuessingGame {
                 gameState = ProcessAttempt(input, gameSetting, gameState);
             }
         }
-        //TODO 検証関数を追加する。
-        //TODO 検証内容に基づいたInterfaceを設定したい
-        public static bool IsValidInput(string? input, out int result) {
-            return int.TryParse(input, out result);
-        }
 
         public static GameState ProcessAttempt(IInputData inputData, GameSetting gameSetting, GameState gameState) {
             // successとかinputは状態なのでいい感じに扱う必要がある。stringは参照型だがイミュータブルなので不変。
-            string? input = null;
-            int parsedInput;
-            while (!IsValidInput(input, out parsedInput)) {
-                input = inputData.InputString();
-            }
-            Console.WriteLine("Your input is valid input");
+            int parsedInput = inputData.InputInteger();
             gameState = GameState.UpdateState(parsedInput, gameState, gameSetting);
-            if (gameState.IsSuccess) {
-                Console.WriteLine("Your input is correct.");
-            }
             return gameState;
         }
     }
 
    public interface IInputData {
        string GetUser();
-       string? InputString();
+       int InputInteger();
        DateTime InputTime();
    }
 
-
+    // Presentation
     public class CliIo : IInputData {
         public string GetUser() {
             return "User";
         }
-
-        public string? InputString() { 
-            Console.WriteLine("Please enter input: ");
-            return Console.ReadLine();
+        //TODO 検証関数を追加する。
+        //TODO 検証内容に基づいたInterfaceを設定したい
+        public static bool IsValidInput(string? input, out int result) {
+            return int.TryParse(input, out result);
         }
 
+        public int InputInteger() {
+            Console.WriteLine("Please enter input: ");
+            string? input = Console.ReadLine();
+            int parsedInput;
+            while (!IsValidInput(input, out parsedInput)) {
+                Console.WriteLine("Invalide Input");
+                input = Console.ReadLine();
+            }
+            return parsedInput;
+        }
         public DateTime InputTime() {
             return DateTime.Now;
         }
