503e54076f8c3feba7f90dec96cbb4a8376a4bfb
norikenpi <norikenpi@gmail.com>
2025-01-02 20:21:41 +0900

    add max attempts

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,21 +2,30 @@
 
 int main(){
   const int SECRET_NUM = 43;
-  std::cout << "Hello World!" << std::endl;
+  const int MAX_ATTEMPTS = 7;
+  int current_attempt = 0;
+  bool is_success = false;
   std::string input;
-  std::cin >> input;
-  std::cout << input << std::endl;
-  try {
-    int num = std::stoi(input);
-    if (SECRET_NUM == num) {
-      std::cout << "Correct" << std::endl;
-    } else {
-      std::cout << "Incorrect" << std::endl;
-    }
-  } catch (std::invalid_argument) {
-    std::cout << "Invalid Input" << std::endl;
-  } catch (const std::out_of_range& e) {
-    std::cout << "Out of range" << std::endl;
-  }
 
+  while (current_attempt < MAX_ATTEMPTS && !is_success) {
+    std::cout << "Input the number" << std::endl;
+    std::cin >> input;
+    try {
+      int num = std::stoi(input);
+      current_attempt++;
+      if (SECRET_NUM == num) {
+        std::cout << "Correct" << std::endl;
+        is_success = true;
+      } else {
+        std::cout << "Incorrect" << std::endl;
+      }
+    } catch (std::invalid_argument) {
+      std::cout << "Invalid Input" << std::endl;
+    } catch (const std::out_of_range& e) {
+      std::cout << "Out of range" << std::endl;
+    }
+  }
+  if (current_attempt == MAX_ATTEMPTS) {
+    std::cout << "max attempts" << std::endl;
+  }
 }
