d6a535d2029307f58dc1169565d8935bb51a18bd
norikenpi <norikenpi@gmail.com>
2025-01-05 17:14:43 +0900

    add cli input publisher

--- cpp/ros2/src/ngg/src/publisher_member_function.cpp
+++ cpp/ros2/src/ngg/src/publisher_member_function.cpp
@@ -2,16 +2,17 @@
 
 MinimalPublisher::MinimalPublisher()
 : Node("minimal_publisher"), count_(0) {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+  publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+  publish_input();
 }
 
-void MinimalPublisher::timer_callback() {
+void MinimalPublisher::publish_input() {
+  for (int i = 0; i < 5 ; i++) {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+    RCLCPP_INFO(this->get_logger(), "Input the number:");
+    std::cin >> message.data;
+    publisher_ -> publish(message);
+  }
 }
 
 int main(int argc, char * argv[]) {
--- cpp/ros2/src/ngg/src/publisher_member_function.hpp
+++ cpp/ros2/src/ngg/src/publisher_member_function.hpp
@@ -16,7 +16,7 @@ public:
   MinimalPublisher();
 
 private:
-  void timer_callback();
+  void publish_input();
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
