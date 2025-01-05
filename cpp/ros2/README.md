abbfa7e4a7576a057619d8e3e2ab55d65e82fb15
norikenpi <norikenpi@gmail.com>
2025-01-05 16:59:59 +0900

    add header and src file

--- cpp/ros2/README.md
+++ cpp/ros2/README.md
@@ -1,9 +1,12 @@
 ```
 make build
 make run
+colcon build
+ros2 run my_package my_node
 ```
 
-ROS2はmsgがinterfaceになっている。
+ROS2はmsgがinterfaceになっていて、pub/sub間の依存関係が分離されている。
+
 
 
 
---
+++ cpp/ros2/src/ngg/src/publisher_member_function.hpp
@@ -0,0 +1,25 @@
+#ifndef PUBLISHER_MEMBER_FUNCTION_HPP_
+#define MINIMAL_PUBLISHER_HPP_
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher();
+
+private:
+  void timer_callback();
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+#endif // MINIMAL_PUBLISHER_HPP_
