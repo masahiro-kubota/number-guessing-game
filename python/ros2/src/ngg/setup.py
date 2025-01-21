17540fd497b1b3e72544a1841838fdfa50ee3bdb
Masahiro Kubota <norikenpi@gmail.com>
2025-01-21 14:17:41 +0900

    add subscriber

--- python/ros2/src/ngg/setup.py
+++ python/ros2/src/ngg/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'talker = ngg.publisher_member_function:main',
+            'listener = ngg.subscriber_member_function:main',
         ],
     },
 )
