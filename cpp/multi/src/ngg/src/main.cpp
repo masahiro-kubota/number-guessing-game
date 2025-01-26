0ac4c191c3cbc326dc162a6ea68fa991c9e791c7
norikenpi <norikenpi@gmail.com>
2025-01-27 01:23:36 +0900

    fix

--- cpp/multi/src/ngg/src/main.cpp
+++ cpp/multi/src/ngg/src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]){
   if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <io_type> " << std::endl;
     return 1;
   }
 
