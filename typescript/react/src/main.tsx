a6a3eb8007e98f71c182b662b43786aa89e946c6
norikenpi <norikenpi@gmail.com>
2025-02-12 01:18:00 +0900

    remove React import

--- typescript/react/src/main.tsx
+++ typescript/react/src/main.tsx
@@ -3,7 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root element not fount");
+}
+const root = createRoot(container);
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
